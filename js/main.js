c5bceaf705f6b70fb18935fe24b28e750e01fd70
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-05-27 15:31:44 -0400

    responsive toggle nav

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+var $navBtn = $('.nav-btn');
+var $menu = $('.nav-list');
+
+$navBtn.on('click', function () {
+	$menu.toggleClass('js-menu-open')
+});
+
 Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
 Galleria.run('.galleria');
