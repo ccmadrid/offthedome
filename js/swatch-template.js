9930b83893f6982b256be90153cdb27a4bdf928d
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-05-30 11:34:32 -0400

    applied fixes to use js DOM for panels

--- js/swatch-template.js
+++ js/swatch-template.js
@@ -18,22 +18,127 @@ Mustache.parse(stripesTemplate);
 var stripesRendered = Mustache.render(stripesTemplate, stripes);
 $('.stripeswatches').html(stripesRendered);
 
-var solidFillTemplate = $('#solid-fill-template').html();
-Mustache.parse(solidFillTemplate);
-var solidFillRendered = Mustache.render(solidFillTemplate, solids);
-$('.solid-fills').html(solidFillRendered);
+// var solidFillTemplate = $('#solid-fill-template').html();
+// Mustache.parse(solidFillTemplate);
+// var solidFillRendered = Mustache.render(solidFillTemplate, solids);
+// $('.solid-fills').html(solidFillRendered);
 
-var patternFillTemplate = $('#pattern-fill-template').html();
-Mustache.parse(patternFillTemplate);
-var patternFillRendered = Mustache.render(patternFillTemplate, patterns);
-$('.pattern-fills').html(patternFillRendered);
+// var patternFillTemplate = $('#pattern-fill-template').html();
+// Mustache.parse(patternFillTemplate);
+// var patternFillRendered = Mustache.render(patternFillTemplate, patterns);
+// $('.pattern-fills').html(patternFillRendered);
 
-var textureFillTemplate = $('#texture-fill-template').html();
-Mustache.parse(textureFillTemplate);
-var textureFillRendered = Mustache.render(textureFillTemplate, textures);
-$('.texture-fills').html(textureFillRendered);
+// var textureFillTemplate = $('#texture-fill-template').html();
+// Mustache.parse(textureFillTemplate);
+// var textureFillRendered = Mustache.render(textureFillTemplate, textures);
+// $('.texture-fills').html(textureFillRendered);
 
-var stripeFillTemplate = $('#stripe-fill-template').html();
-Mustache.parse(stripeFillTemplate);
-var stripeFillRendered = Mustache.render(stripeFillTemplate, stripes);
-$('.stripe-fills').html(stripeFillRendered);
+// var stripeFillTemplate = $('#stripe-fill-template').html();
+// Mustache.parse(stripeFillTemplate);
+// var stripeFillRendered = Mustache.render(stripeFillTemplate, stripes);
+// $('.stripe-fills').html(stripeFillRendered);
+
+// Find the <def> tag for the solid fills
+var solidFills = document.querySelector('.solid-fills');
+
+// Loop over all the solid fills from the `swatch-data.js` file
+solids.solids.forEach(function (elem, index) {
+  // Create a new SVG `pattern` element
+  var pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+  // Populate its attributes with the correct information
+  pattern.setAttribute('id', elem.swatchId);
+  pattern.setAttribute('patternUnits', 'userSpaceOnUse');
+  pattern.setAttribute('width', 125);
+  pattern.setAttribute('height', 125);
+
+  // Create a new SVG `image` element
+  var image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  // Populate its attributes with the correct information
+  image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'img/' + elem.swatchId + '.jpg');
+  image.setAttribute('width', 125);
+  image.setAttribute('height', 125);
+
+  // Add the <image> tag into the <pattern> tag
+  pattern.appendChild(image);
+  // Add the <pattern> tag into the <defs> tag
+  solidFills.appendChild(pattern);
+});
+
+
+// Find the <def> tag for the pattern fills
+var patternFills = document.querySelector('.pattern-fills');
+
+// Loop over all the pattern fills from the `swatch-data.js` file
+patterns.patterns.forEach(function (elem, index) {
+  // Create a new SVG `pattern` element
+  var pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+  // Populate its attributes with the correct information
+  pattern.setAttribute('id', elem.swatchId);
+  pattern.setAttribute('patternUnits', 'userSpaceOnUse');
+  pattern.setAttribute('width', 125);
+  pattern.setAttribute('height', 125);
+
+  // Create a new SVG `image` element
+  var image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  // Populate its attributes with the correct information
+  image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'img/' + elem.swatchId + '.jpg');
+  image.setAttribute('width', 125);
+  image.setAttribute('height', 125);
+
+  // Add the <image> tag into the <pattern> tag
+  pattern.appendChild(image);
+  // Add the <pattern> tag into the <defs> tag
+  patternFills.appendChild(pattern);
+});
+
+// Find the <def> tag for the texture fills
+var textureFills = document.querySelector('.texture-fills');
+
+// Loop over all the texture fills from the `swatch-data.js` file
+textures.textures.forEach(function (elem, index) {
+  // Create a new SVG `pattern` element
+  var pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+  // Populate its attributes with the correct information
+  pattern.setAttribute('id', elem.swatchId);
+  pattern.setAttribute('patternUnits', 'userSpaceOnUse');
+  pattern.setAttribute('width', 125);
+  pattern.setAttribute('height', 125);
+
+  // Create a new SVG `image` element
+  var image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  // Populate its attributes with the correct information
+  image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'img/' + elem.swatchId + '.jpg');
+  image.setAttribute('width', 125);
+  image.setAttribute('height', 125);
+
+  // Add the <image> tag into the <pattern> tag
+  pattern.appendChild(image);
+  // Add the <pattern> tag into the <defs> tag
+  textureFills.appendChild(pattern);
+});
+
+// Find the <def> tag for the stripe fills
+var stripeFills = document.querySelector('.stripe-fills');
+
+// Loop over all the texture fills from the `swatch-data.js` file
+stripes.stripes.forEach(function (elem, index) {
+  // Create a new SVG `pattern` element
+  var pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+  // Populate its attributes with the correct information
+  pattern.setAttribute('id', elem.swatchId);
+  pattern.setAttribute('patternUnits', 'userSpaceOnUse');
+  pattern.setAttribute('width', 125);
+  pattern.setAttribute('height', 125);
+
+  // Create a new SVG `image` element
+  var image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  // Populate its attributes with the correct information
+  image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'img/' + elem.swatchId + '.jpg');
+  image.setAttribute('width', 125);
+  image.setAttribute('height', 125);
+
+  // Add the <image> tag into the <pattern> tag
+  pattern.appendChild(image);
+  // Add the <pattern> tag into the <defs> tag
+  stripeFills.appendChild(pattern);
+});
